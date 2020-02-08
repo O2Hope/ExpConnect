95deb79dc59a06808a5449db902e7ae1577c536a
Agustin Ruiz <agusvaldes2@live.com>
2020-02-08 15:18:58 -0600

    Add User model

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ connectDB();
 app.get("/", (req, res) => res.send("API Running"));
 
 //Define Routes
-
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
