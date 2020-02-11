6c0e7e44fac8e7b09c0c0fba07eedd8871bd56bc
Agustin Ruiz <agusvaldes2@live.com>
2020-02-10 21:57:27 -0600

    Create client and initial sructure

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import { Button } from "antd";
+
+function App() {
+  return (
+    <Fragment>
+      <h1>Hello world!</h1>
+      <Button type="primary">Hello world!</Button>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ client/src/components/layout/navbar/navbar.component.jsx
@@ -0,0 +1,4 @@
+import React, { Fragment } from "react";
+const { Header, Content, Footer } = Layout;
+
+const Navbar = () => <Fragment></Fragment>;
